0182b53a2e5926655b9241a351aac28430736e3a
tuanmaicong <maicongtuannn@gmail.com>
2024-06-27 10:01:47 +0700

    update_layout

--- resources/views/fe/layouts/components/navbar.blade.php
+++ resources/views/fe/layouts/components/navbar.blade.php
@@ -12,53 +12,10 @@
                 <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a href="#" class="nav-link dropdown-toggle active">Home <i class="bx bx-chevron-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item">
-                                    <a href="index.html" class="nav-link">Home Demo One</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index-2.html" class="nav-link">Home Demo Two</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index-3.html" class="nav-link">Home Demo Three</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="index-4.html" class="nav-link active">Home Demo Four (Revolution)</a>
-                                </li>
-                            </ul>
+                            <a href="#" class="nav-link dropdown-toggle active">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link dropdown-toggle">Pages <i class="bx bx-chevron-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link dropdown-toggle">Users <i
-                                            class="bx bx-chevron-down"></i></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="nav-item">
-                                            <a href="login.html" class="nav-link">Login</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="register.html" class="nav-link">Register</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="faq.html" class="nav-link">FAQ</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="404.html" class="nav-link">404 Error Page</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="return-policy.html" class="nav-link">Return Policy</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="terms-conditions.html" class="nav-link">Terms & Conditions</a>
-                                </li>
-                            </ul>
+                            <a href="#" class="nav-link dropdown-toggle">Pages</a>
                         </li>
                         <li class="nav-item">
                             <a href="about.html" class="nav-link">About Us</a>
@@ -77,29 +34,6 @@
                                 </li>
                                 <li class="nav-item">
                                     <a href="blog-details.html" class="nav-link">Blog Details</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link dropdown-toggle">Shop <i class="bx bx-chevron-down"></i></a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item">
-                                    <a href="shop.html" class="nav-link">Shop</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="checkout.html" class="nav-link">Checkout</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="single-product.html" class="nav-link">Single Product</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="products-on-sale.html" class="nav-link">Products On Sale</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="order-tracking.html" class="nav-link">Order Tracking</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="customer-service.html" class="nav-link">Customer Service</a>
                                 </li>
                             </ul>
                         </li>
